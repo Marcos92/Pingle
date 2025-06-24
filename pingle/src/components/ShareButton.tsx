2b210e3ea54d5f4853d4ab2c09cf7f75561294c4
Marcos Rodrigues <marcos.mgbr@gmail.com>
2025-06-24 17:25:07 +0100

    Improve share

--- pingle/src/components/ShareButton.tsx
+++ pingle/src/components/ShareButton.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
 type ShareButtonProps = {
   share: () => void;
 };
 
 export default function ShareButton({ share }: ShareButtonProps) {
+  const [copied, setCopied] = useState(false);
+
   function handleInput() {
     share();
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1000);
   }
 
   return (
-    <>
-      <button className="share-button" onClick={handleInput}>
-        Partilhar<i className="fa fa-share-nodes"></i>
-      </button>
-    </>
+    <button className="share-button" onClick={handleInput}>
+      {copied ? (
+        "Copiado com sucesso!"
+      ) : (
+        <>
+          Partilhar <i className="fa fa-share-nodes"></i>
+        </>
+      )}
+    </button>
   );
 }
